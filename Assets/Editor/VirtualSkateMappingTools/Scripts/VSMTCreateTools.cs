d7eb9314eae9ab984ddd9d4c71dce40a7effde8f
Ramon Meza <ramon_meza@live.com>
2025-06-13 00:04:11 -0400

    Add ability to create maps from scenetemplate.

---
+++ Assets/Editor/VirtualSkateMappingTools/Scripts/VSMTCreateTools.cs
@@ -0,0 +1,68 @@
+﻿using UnityEditor;
+using UnityEditor.SceneTemplate;
+using UnityEngine;
+
+
+namespace VirtualSkateMappingTools
+{
+    public class VSMTCreateTools
+    {
+        public delegate void MapCreationCompletedHandler(bool mapCreationSuccessful);
+        public static event MapCreationCompletedHandler MapCreationCompleted;
+        private static bool _isBuilding = false;
+
+        public static void CreateNewMap(string mapName)
+        {
+            var mapCreationResult = false;
+            try
+            {
+
+                if (_isBuilding)
+                {
+                    Debug.LogWarning("Map creation already in progress!");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(mapName))
+                {
+                    Debug.LogError("Please provide a valid map name");
+                    return;
+                }
+
+                Debug.Log("Attempting to create a new map...");
+                _isBuilding = true;
+
+                var mapNameCleaned = mapName.Replace(" ", "_");
+                var assetBundleName = mapNameCleaned.ToLower();
+
+                var template = AssetDatabase.LoadAssetAtPath<SceneTemplateAsset>("Assets/Editor/VirtualSkateMappingTools/VSMT.scenetemplate");
+                string newScenePath = $"Assets/Maps/{mapNameCleaned}/{mapNameCleaned}.unity";
+
+                var result = SceneTemplateService.Instantiate(template, false, newScenePath);
+                if (result != null)
+                {
+                    AssetImporter sceneImporter = AssetImporter.GetAtPath(newScenePath);
+                    if (sceneImporter != null)
+                    {
+                        sceneImporter.assetBundleName = assetBundleName;
+                        sceneImporter.SaveAndReimport();
+                        Debug.Log($"Map created successful: {newScenePath} (AssetBundle: {sceneImporter.assetBundleName})");
+                    }
+                    else
+                    {
+                        Debug.LogError($"Failed to get AssetImporter for new map at: {newScenePath}");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Failed to create map from template");
+                }
+            }
+            finally
+            {
+                _isBuilding = false;
+                MapCreationCompleted?.Invoke(mapCreationResult);
+            }
+        }
+    }
+}
--- Assets/Editor/VirtualSkateMappingTools/Scripts/VSMTEditorWindow.cs
+++ Assets/Editor/VirtualSkateMappingTools/Scripts/VSMTEditorWindow.cs
@@ -30,24 +30,52 @@ namespace VirtualSkateMappingTools
             var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(assetPath);
             rootVisualElement.styleSheets.Add(styleSheet);
 
-            var group = new GroupBox("Build");
+            var group = new GroupBox("Create");
+            var mapNameField = new TextField("Map Name");
+            group.Add(mapNameField);
+            var createMapBtn = new Button();
+            createMapBtn.text = "Create New Map";
+            createMapBtn.clicked += () =>
+            {
+                try
+                {
+                    createMapBtn.SetEnabled(false);
+                    VSMTCreateTools.CreateNewMap(mapNameField.value);
+                }
+                finally
+                {
+                    createMapBtn.SetEnabled(true);
+                }
+            };
+            group.Add(createMapBtn);
+            rootVisualElement.Add(group);
+
+            group = new GroupBox("Build");
             var buildBtn = new Button();
             buildBtn.text = "Build Currently Loaded Map";
             buildBtn.clicked += () =>
             {
                 buildBtn.SetEnabled(false);
-                VSMTBuildTools.BuildCurrentMap();
-                buildBtn.SetEnabled(true);
+                try
+                {
+                    VSMTBuildTools.BuildCurrentMap();
+                }
+                finally
+                {
+                    buildBtn.SetEnabled(true);
+                }
             };
             group.Add(buildBtn);
             rootVisualElement.Add(group);
 
-            group = new GroupBox("Copy to Mod Maps Directory");
+            group = new GroupBox("Play");
             _assetBundleScrollView = new ScrollView();
             group.Add(_assetBundleScrollView);
             rootVisualElement.Add(group);
 
             VSMTUtilities.OnAssetBundlesChanged += UpdateAssetBundleScrollView;
+            VSMTBuildTools.BuildCompleted += (_) => UpdateAssetBundleScrollView();
+            EditorApplication.projectChanged += UpdateAssetBundleScrollView;
         }
 
         private void UpdateAssetBundleScrollView()
@@ -60,13 +88,21 @@ namespace VirtualSkateMappingTools
             _assetBundleScrollView.Clear();
             foreach (var assetBundle in AssetDatabase.GetAllAssetBundleNames())
             {
+                var mapPath = Path.Combine(Application.dataPath, "../AssetBundles", assetBundle);
+                if (!File.Exists(mapPath))
+                {
+                    continue;
+                }
+
                 var btn = new Button(() =>
                 {
                     VSMTUtilities.CopyAssetBundleToGameDir(assetBundle);
                 });
-                btn.text = assetBundle;
+                btn.text = $"Copy {assetBundle} to Mod Maps Directory";
                 _assetBundleScrollView.Add(btn);
             }
+
+            Repaint();
         }
     }
 }
